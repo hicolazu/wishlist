f75b00effef4573dcec976e5d96117a8da884038
Henrico Lazuroz <henrico.almeida@americanas.io>
2023-06-18 14:29:00 -0300

    fix: Adjusting product sku to Id

--- src/main/java/com/lazuroz/wishlist/dataprovider/repository/entity/ProductEntity.java
+++ src/main/java/com/lazuroz/wishlist/dataprovider/repository/entity/ProductEntity.java
@@ -2,10 +2,12 @@ package com.lazuroz.wishlist.dataprovider.repository.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
 public class ProductEntity {
+    @Id
     private String sku;
     private String name;
     private String description;
