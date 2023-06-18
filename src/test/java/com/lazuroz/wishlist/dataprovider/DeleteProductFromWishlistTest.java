13c127f56128679ef958d86e5cc50b02b39f9338
Henrico Lazuroz <henrico.almeida@americanas.io>
2023-06-18 17:03:33 -0300

    test: Adding delete product from wishilist dataprovider test

--- src/test/java/com/lazuroz/wishlist/dataprovider/DeleteProductFromWishlistTest.java
+++ src/test/java/com/lazuroz/wishlist/dataprovider/DeleteProductFromWishlistTest.java
@@ -1,6 +1,7 @@
 package com.lazuroz.wishlist.dataprovider;
 
 import com.lazuroz.wishlist.AbstractIntegrationTest;
+import com.lazuroz.wishlist.core.dataprovider.DeleteProductFromWishlist;
 import com.lazuroz.wishlist.core.dataprovider.InsertWishlistProduct;
 import com.lazuroz.wishlist.core.domain.builder.CustomerBuilder;
 import com.lazuroz.wishlist.core.domain.builder.ProductBuilder;
@@ -10,11 +11,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class InsertWishlistProductTest extends AbstractIntegrationTest {
+class DeleteProductFromWishlistTest extends AbstractIntegrationTest {
+
+    @Autowired
+    DeleteProductFromWishlist deleteProductFromWishlist;
 
     @Autowired
     InsertWishlistProduct insertWishlistProduct;
@@ -28,7 +30,7 @@ class InsertWishlistProductTest extends AbstractIntegrationTest {
     }
 
     @Test
-    void GivenNewWishlistAndProduct_whenInsertingAProduct_thenTheProductIsAddedToTheWishlist() {
+    void GivenWishlistWithProduct_whenDeletingAProduct_thenTheProductIsRemovedFromTheWishlist() {
         // Given
         var customer = new CustomerBuilder()
                 .withId("1")
@@ -45,58 +47,60 @@ class InsertWishlistProductTest extends AbstractIntegrationTest {
                 .build();
 
         var wishlist = new WishlistBuilder()
-                .withId(null)
+                .withId("1")
                 .withCustomer(customer)
                 .build();
 
-        // When
         insertWishlistProduct.insert(wishlist, product);
+
+        // When
+        deleteProductFromWishlist.delete(wishlist, product);
+
+        // Then
+        var wishlistEntity = wishlistRepository.findByCustomerId(wishlist.getCustomer().getId()).get();
+        assertEquals(0, wishlistEntity.getProducts().size());
+        assertEquals(wishlist.getCustomer().getId(), wishlistEntity.getCustomer().getId());
+    }
+
+    @Test
+    void GivenWishlistWithProduct_whenDeletingANonWishlistProduct_thenTheProductIsNotRemovedFromTheWishlist() {
+        // Given
+        var customer = new CustomerBuilder()
+                .withId("1")
+                .withName("John")
+                .withEmail("john@mail.com")
+                .withCpf("12345678900")
+                .build();
+
+        var product = new ProductBuilder()
+                .withSku("1")
+                .withName("Product 1")
+                .withDescription("Description 1")
+                .withPrice(10.0)
+                .build();
+
+        var wishlist = new WishlistBuilder()
+                .withId("1")
+                .withCustomer(customer)
+                .build();
+
+        insertWishlistProduct.insert(wishlist, product);
+
+        // When
+        var product2 = new ProductBuilder()
+                .withSku("2")
+                .withName("Product 2")
+                .withDescription("Description 2")
+                .withPrice(4.0)
+                .build();
+
+        deleteProductFromWishlist.delete(wishlist, product2);
 
         // Then
         var wishlistEntity = wishlistRepository.findByCustomerId(wishlist.getCustomer().getId()).get();
         assertEquals(1, wishlistEntity.getProducts().size());
-        assertEquals(product.getSku(), wishlistEntity.getProducts().get(0).getSku());
-        assertEquals(customer.getId(), wishlistEntity.getCustomer().getId());
-        assertNotNull(wishlistEntity.getId());
+        assertEquals(wishlist.getCustomer().getId(), wishlistEntity.getCustomer().getId());
     }
 
-    @Test
-    void GivenANonEmptyWishlistAndAProduct_whenInsertingAProduct_thenTheProductIsAddedToTheWishlist() {
-        // Given
-        var customer = new CustomerBuilder()
-                .withId("1")
-                .withName("John")
-                .withEmail("john@mail.com")
-                .withCpf("12345678900")
-                .build();
 
-        var product = new ProductBuilder()
-                .withSku("1")
-                .withName("Product 1")
-                .withDescription("Description 1")
-                .withPrice(10.0)
-                .build();
-
-        var wishlist = new WishlistBuilder()
-                .withId("1")
-                .withCustomer(customer)
-                .build();
-
-        insertWishlistProduct.insert(wishlist, product);
-
-        var newProduct = new ProductBuilder()
-                .withSku("2")
-                .withName("Product 2")
-                .withDescription("Description 2")
-                .withPrice(5.0)
-                .build();
-
-        // When
-        insertWishlistProduct.insert(wishlist, newProduct);
-
-        // Then
-        var wishlistEntity = wishlistRepository.findByCustomerId(wishlist.getCustomer().getId()).get();
-        assertEquals(2, wishlistEntity.getProducts().size());
-        assertEquals(customer.getId(), wishlistEntity.getCustomer().getId());
-    }
 }
